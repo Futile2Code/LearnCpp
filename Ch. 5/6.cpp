c7d25997f3e948357f8e5e5333d8888445675170
Bayou M <mbayou75@gmail.com>
2024-07-24 15:41:53 +0100

    6 fixed

--- Ch. 5/6.cpp
+++ Ch. 5/6.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string_view>
 
 // Write the function getQuantityPhrase() here
 constexpr std::string_view getQuantityPhrase(const int count)
@@ -35,14 +36,16 @@ constexpr std::string_view getApplesPluralized(const int count)
 
 int main()
 {
+    using namespace std::string_view_literals;
+    
     constexpr int maryApples { 3 };
-    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+    std::cout << "Mary has "sv << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
 
-    std::cout << "How many apples do you have? ";
+    std::cout << "How many apples do you have? "sv;
     int numApples{};
     std::cin >> numApples;
 
-    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+    std::cout << "You have "sv << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
 
     return 0;
 }
